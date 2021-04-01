055bb57bcd0490739df4d7871189de39f96d4c08
FajarTheGGman <newstrooper731@gmail.com>
2021-04-01 20:22:19 +0700

    Massive update

--- jalankan.sh
+++ jalankan.sh
@@ -12,8 +12,8 @@ echo -e $white' \_^_/\n'
 echo -e $green"{ Coder: 'Fajar Firdaus' }\n"
 
 echo -e $yellow'[/] Tunggu sebentar..';
-#composer install
-#echo -e $green'[/] Menginstall package selesai'
+composer install
+echo -e $green'[/] Menginstall package selesai'
 ./artisan db:create inventory
 echo -e $green '[+] Membuat Database Selesai..'
 ./artisan migrate 
